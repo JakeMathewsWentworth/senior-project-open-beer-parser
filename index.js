47a207d1a8a147b1185f862b249c9d8dd3d46b06
Jake Mathews <jake.mathews.email@gmail.com>
2018-07-18 10:30:00 -0400

    Fixed typo

--- index.js
+++ index.js
@@ -15,26 +15,48 @@ var firestore = new Firestore({
 var unkownValue = '';
 
 var beerdb = require(args.beerdb);
+var categories = firestore.collection('categories');
+var styles = firestore.collection('styles');
 var beers = firestore.collection('beers');
 var brewerys = firestore.collection('brewers');
 
-var beerCount = 0;
-var invalidBeerCount = 0;
-var breweryCount = 0;
-var invalidBreweryCount = 0;
+var invalid = {
+    beer: 0,
+    brewery: 0,
+    style: 0,
+    category: 0
+}
+var valid = {
+    beer: 0,
+    brewery: 0,
+    style: 0,
+    category: 0
+}
+
 for (beerIndex in beerdb) {
     var beer = beerdb[beerIndex].fields;
+
+    var categoryId = beer.cat_id ? beer.cat_id : unkownValue;
+    var category = {
+        name: beer.cat_name ? beer.cat_name : unkownValue,
+    }
+
+    var styleId = beer.style_id ? beer.style_id : unkownValue;
+    var style = {
+        name: beer.style_name ? beer.style_name : unkownValue,
+    }
+
     var finalBeer = {
         name: beer.name ? beer.name : unkownValue,
-        category: beer.cat_name ? beer.cat_name : unkownValue,
-        style: beer.style_name ? beer.style_name : unkownValue,
+        categoryId: categoryId,
+        styleId: styleId,
         description: beer.descript ? beer.descript : unkownValue,
         abv: beer.abv ? beer.abv : unkownValue,
         ibu: beer.ibu ? beer.ibu : unkownValue,
         upc: beer.upc ? beer.upc : unkownValue,
-        brewery_id: beer.brewery_id ? beer.brewery_id : unkownValue
+        breweryId: beer.brewery_id ? beer.brewery_id : unkownValue
     }
-    
+
     var finalBrewery = {
         name: beer.name_breweries ? beer.name_breweries : unkownValue,
         country: beer.country ? beer.country : unkownValue,
@@ -46,24 +68,43 @@ for (beerIndex in beerdb) {
         website: beer.website ? beer.website : unkownValue,
     }
 
+    if (isNaN(categoryId) || !categoryId) {
+        console.log("Invalid category ID", categoryId);
+        invalid.category += 1;
+    } else {
+        categories.doc(categoryId).set(category);
+        valid.category += 1;
+    }
+
+    if (isNaN(styleId) || !styleId) {
+        console.log("Invalid style ID", styleId);
+        invalid.style += 1;
+    } else {
+        styles.doc(styleId).set(style);
+        valid.style += 1;
+    }
+
     if (isNaN(beer.id)) {
         console.log("Invalid beer ID", beer.id);
-        invalidBeerCount++;
+        invalid.beer += 1;
     } else {
         beers.doc(beer.id).set(finalBeer);
-        beerCount++;
+        valid.beer += 1;
     }
 
     if (isNaN(beer.brewery_id)) {
         console.log("Invalid brewery ID", beer.brewery_id);
-        invalidBreweryCount++;
+        invalid.brewery += 1;
     } else {
         brewerys.doc(beer.brewery_id).set(finalBrewery);
-        breweryCount++;
+        valid.brewery += 1;
     }
 }
 console.log('Done... Summary Below');
-console.log('Beer Count', beerCount);
-console.log('Brewery Count', breweryCount);
-console.log('Invalid Beer Count', invalidBeerCount);
-console.log('Invalid Brewery Count', invalidBreweryCount);
+console.log(valid);
+console.log(invalid);
+console.log('Beer Count', valid.beer);
+console.log('Brewery Count', valid.brewery);
+console.log('Invalid Beer Count', invalid.beer);
+console.log('Invalid Brewery Count', invalid.brewery);
+console.log('Uploading Data Now...')
